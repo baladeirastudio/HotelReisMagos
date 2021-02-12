08162ea57f1082df9893ce564ed815c2db0101cc
Gabriel "fr4ct1ons" Lucena <gabriellucena23@gmail.com>
2021-02-11 23:07:16 -0300

    Importing FizzySteamworks

--- HotelReisMagos - Project/Assets/Scripts/PlayerInfo.cs
+++ HotelReisMagos - Project/Assets/Scripts/PlayerInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System;
 using Mirror;
 using Mirror.Authenticators;
+using Steamworks;
 using TMPro;
 using UnityEngine;
 
@@ -8,9 +9,15 @@ public class PlayerInfo : MonoBehaviour
 {
     [SerializeField] private string playerName = "Player";
     [SerializeField] private TMP_InputField nameInput;
+    [SerializeField] private bool useSteam = false;
+
+    protected Callback<LobbyCreated_t> lobbyCreated;
+    protected Callback<GameLobbyJoinRequested_t> gameLobbyJoinRequest;
+    protected Callback<LobbyEnter_t> lobbyEnter;
     
     public static string clientName;
-    
+    [SerializeField] private string steamID;
+
     private void Awake()
     {
         if (PlayerPrefs.HasKey("CurrentPlayerName"))
@@ -21,6 +28,56 @@ public class PlayerInfo : MonoBehaviour
         }
     }
 
+    private void Start()
+    {
+        if (!useSteam)
+        {
+            return;
+        }
+        else
+        {
+            lobbyCreated = Callback<LobbyCreated_t>.Create(OnLobbyCreated);
+            gameLobbyJoinRequest = Callback<GameLobbyJoinRequested_t>.Create(OnGameLobbyJoinRequest);
+            lobbyEnter = Callback<LobbyEnter_t>.Create(OnLobbyEntered);
+        }
+    }
+
+    private void OnLobbyEntered(LobbyEnter_t callback)
+    {
+        if (NetworkServer.active) //Means the executing client is the host.
+        {
+            return;
+        }
+        else
+        {
+            string hostAddress = SteamMatchmaking.GetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), 
+                "HostAddress");
+
+            NetworkManager.singleton.networkAddress = hostAddress;
+            NetworkManager.singleton.StartClient();
+        }
+    }
+
+    private void OnGameLobbyJoinRequest(GameLobbyJoinRequested_t callback)
+    {
+        SteamMatchmaking.JoinLobby(callback.m_steamIDLobby);
+    }
+
+    private void OnLobbyCreated(LobbyCreated_t callback)
+    {
+        if (callback.m_eResult != EResult.k_EResultOK)
+        {
+            Debug.LogError("Failed to create lobby!");
+            return;
+        }
+        else
+        {
+            NetworkManager.singleton.StartHost();
+            SteamMatchmaking.SetLobbyData(new CSteamID(callback.m_ulSteamIDLobby), "HostAddress",
+                SteamUser.GetSteamID().ToString());
+        }
+    }
+
     public void UpdatePlayerName (string newName)
     {
         playerName = newName;
@@ -28,14 +85,34 @@ public class PlayerInfo : MonoBehaviour
         PlayerPrefs.SetString("CurrentPlayerName", playerName);
         PlayerPrefs.Save();
     }
+    
+    public void UpdateSteamID (string newID)
+    {
+        steamID = newID;
+
+        /*playerName = newName;
+        clientName = playerName;
+        PlayerPrefs.SetString("CurrentPlayerName", playerName);
+        PlayerPrefs.Save();*/
+    }
 
     public void Connect()
     {
+        NetworkManager.singleton.networkAddress = steamID;
         NetworkManager.singleton.StartClient();
     }
     
     public void Host()
     {
-        NetworkManager.singleton.StartHost();
+        if (useSteam)
+        {
+            Debug.Log("USING STEAM");
+            SteamMatchmaking.CreateLobby(ELobbyType.k_ELobbyTypeFriendsOnly, 4);
+            return;
+        }
+        else
+        {
+            NetworkManager.singleton.StartHost();
+        }
     }
 }
