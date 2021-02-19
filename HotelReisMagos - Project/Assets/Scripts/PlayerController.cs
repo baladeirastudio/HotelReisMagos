8990b5e5f4d18d425672d07a3caee6ddf0052e3d
João Vitor <jv.albuquerque@live.com>
2021-02-19 11:21:46 -0300

    Minior changes

---
+++ HotelReisMagos - Project/Assets/Scripts/PlayerController.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    [SerializeField] private int playerNumber;
+
+    private bool isMyTurn = false;
+
+    public int PlayerNumber { get => playerNumber; set => playerNumber = value; }
+
+    private void Start()
+    {
+        GameController.instance.AddPlayer(this);
+    }
+
+    public void IsYourTurn()
+    {
+        isMyTurn = true;
+    }
+}
