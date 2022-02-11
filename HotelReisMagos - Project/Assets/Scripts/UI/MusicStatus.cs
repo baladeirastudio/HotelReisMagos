a095155dbb602473b30b4a3d55919beb6ba4f1b0
Samir Rodrigues <emaildesamir@gmail.com>
2022-02-10 22:03:14 -0300

    Fix Update

--- HotelReisMagos - Project/Assets/Scripts/UI/MusicStatus.cs
+++ HotelReisMagos - Project/Assets/Scripts/UI/MusicStatus.cs
@@ -2,10 +2,19 @@
 using System.Collections.Generic;
 using UnityEngine;
 
-public class MusicStatus : MonoBehaviour
+public class MusicStatus : MonoBehaviour     
 {
+    public new AudioSource audio;
+    public float value;
+
+    private void Start()
+    {
+        OnChange(true);
+    }
+   
+
     public void OnChange(bool val)
     {
-        AudioListener.volume = val ? 1.0f : 0.0f;
+        audio.volume = val ? value : 0.0f;
     }
 }
